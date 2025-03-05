325a8481fbb9f73d91cfed7f69e56b249ac86f03
uguuy <lila.sawhney18@gmail.com>
2025-03-05 09:58:44 +0100

    Initial commit

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask on Kali!"
+    return "Hello world!"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
+    app.run(debug=True)
 
